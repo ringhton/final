8d43695e99bb7d7af090986d68c6b85926df075f
ringhton555 <ringhton555@gmail.com>
2024-05-17 21:23:52 +0000

    Work version
    Create 2 compose file, kibana and app work through traefik

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker-compose -f app.compose.yml -p rbmdkrfinalapp up -d
-docker-compose -f efk.compose.yml -p rbmdkrfinalefk up -d
+docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
+docker compose -f app.compose.yml -p rbmdkrfinalapp up -d
+sleep 45 && ./entrypoint
