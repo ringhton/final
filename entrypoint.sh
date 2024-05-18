a717e60cfc2058e2830c686253bc6953092642cc
ringhton555@gmail.com <user@epd3qkb8bmhvqmjl07od.auto.internal>
2024-05-18 09:48:32 +0000

    Update scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,1 @@
-#mysql -u ${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /root/telemetry_mysql.sql
-#docker exec -ti final-mysql-1 'mysql -u speed --password=test speedtest_telemetry < /root/telemetry_mysql.sql'
-cat speed/results/telemetry_mysql.sql | docker exec -i final-mysql-1 mysql -u speed --password=test speedtest_telemetry
+cat speed/results/telemetry_mysql.sql | docker exec -i rbmdkrfinalapp-mysql-1 mysql -u speed --password=test speedtest_telemetry
--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker network create traefik-public
 docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
 docker compose -f app.compose.yml -p rbmdkrfinalapp up -d
-sleep 45 && ./entrypoint
+sleep 5 && ./entrypoint.sh
